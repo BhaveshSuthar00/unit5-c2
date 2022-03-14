1708a24e2f9161f7ccefbf0ef0b2a24d6bd73f95
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-03-14 09:42:54 +0530

    started

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Rentals } from "./Components/Rentals/Rentals";
+import { AddHouse } from "./Components/AddHouse/AddHouse";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="toggleForm">
+      < AddHouse />
+        {/* Show text Add House or Show Rentals based on state */}
+      </button>
+        < Rentals />
+
+      {/* Show component based on state */}
+      <br />
+    </div>
+  );
+}
+
+export default App;
